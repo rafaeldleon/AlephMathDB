8c9e722e83d7019077dabf1acaeb88b6f8412ca0
rafaeldleon <rafaeldleon@gmail.com>
2010-08-03 18:06:28 -0500

    Uploading data to the DB

--- alephmathdbserver/alephmathdbserver/controllers/uploadobject.py
+++ alephmathdbserver/alephmathdbserver/controllers/uploadobject.py
@@ -5,6 +5,8 @@ from pylons.controllers.util import abort, redirect
 
 from alephmathdbserver.lib.base import BaseController, render
 
+from pymongo import Connection
+from datetime import datetime
 log = logging.getLogger(__name__)
 
 class UploadobjectController(BaseController):
@@ -12,8 +14,15 @@ class UploadobjectController(BaseController):
     def index(self):
         # Return a rendered template
         return render('/uploadobject.mako')
-        # or, return a string
-        #return 'Hello World'
 
     def email(self):
-        return 'Your email is: %s' % request.params['email']
+        posts={};
+        for key in request.params:
+            posts[key]=request.params[key]
+        posts['date_of_creation']=datetime.utcnow()
+        con = Connection('localhost', 27017)
+        db = con.testdb
+        collection = db.test_collection
+        collection.insert(posts)
+        #return 'Your email is: %s' % request.params['email']
+        return 'Added in DB'
